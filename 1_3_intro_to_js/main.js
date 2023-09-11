ef7bb80ef3ca393ff4378f21afdd8fbe07ce69bf
Kelechi Iwuagwu <kayceeprag@gmail.com>
2023-09-11 17:35:52 -0400

    update homework

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('countButton');
+    const counter = document.getElementById('counter');
+    let clickCount = 0;
+    button.addEventListener('click', () => {
+            clickCount++;
+        counter.innerText = clickCount;
+    });
+
+    const nameInput = document.getElementById('name');
+    const ageInput = document.getElementById('age');
+    const emailInput = document.getElementById('email');
+    const submitButton = document.getElementById('submitButton');
+    const displayInfo = d3.select('#displayInfo');
+
+    const userInfo = {
+        name: '',
+        age: '',
+        email: ''
+    };
+
+    submitButton.addEventListener('click', () => {
+
+        userInfo.name = nameInput.value;
+        userInfo.age = ageInput.value;
+        userInfo.email = emailInput.value;
+
+        displayInfo.html('');
+
+        displayInfo.append('p').text(`Name: ${userInfo.name}`);
+        displayInfo.append('p').text(`Age: ${userInfo.age}`);
+        displayInfo.append('p').text(`Email: ${userInfo.email}`);
+    });
+});
